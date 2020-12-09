8207e4f57a6b19c1ac25bdd8322be960ede917d0
wrrrrrw420 <71092490+wrrrrrw420@users.noreply.github.com>
2020-12-09 23:21:44 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express")
 var app = express()
-var PORT = process.env.PORT || 3000;
 const PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var bodyParser = require("body-parser")
 
 app.use(express.static('static'))
